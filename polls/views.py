42472028061215f11103edc38fd3ea7bdc335473
Bedirhan Sahin <bedirhannsahin@gmail.com>
2022-08-05 17:12:05 +0300

    results.html added

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from . models import Question
+from . models import Question, Choice
+from django.urls import reverse
 
 
 def index(request):
@@ -21,8 +22,25 @@ def detail(request, question_id):
         return render(request, 'polls/detail.html', context)
     
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question':question,
+    }
 
+    return render(request, 'polls/results.html', context)
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)    
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question':question,
+        'error_message': "You didn't select a choice."
+     }
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(request, context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
